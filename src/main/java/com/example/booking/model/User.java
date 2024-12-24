c0c8e0e7bd8d6d82cd4a41a1dec2a3bf095d2bbe
salmanshaikssk007 <salmanshaikssk007@gmail.com>
2024-12-24 14:34:15 -0600

    Added user Entity

--- src/main/java/com/example/booking/model/User.java
+++ src/main/java/com/example/booking/model/User.java
@@ -1,15 +1,33 @@
 package com.example.booking.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+//    constructors for the user entity
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
 
 }
